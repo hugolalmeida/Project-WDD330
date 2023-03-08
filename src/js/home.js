3deb90a65aeba6dd81f5e6a9be188cb877e64afe
hugolalmeida <hugogule@gmail.com>
2023-03-08 17:18:42 -0300

    started home page

---
+++ src/js/home.js
@@ -0,0 +1,45 @@
+// let slideIndex = 1;
+// showSlides(slideIndex);
+
+// // Next/previous controls
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
+
+// // Thumbnail image controls
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
+
+// function showSlides(n) {
+//   let i;
+//   let slides = document.getElementsByClassName("mySlides");
+//   let dots = document.getElementsByClassName("dot");
+//   if (n > slides.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex-1].style.display = "block";
+//   dots[slideIndex-1].className += " active";
+// }
+
+let slideInd = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideInd++;
+  if (slideInd > slides.length) {
+    slideInd = 1;
+  }
+  slides[slideInd - 1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: "src/",
+
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        cart: resolve(__dirname, "src/cart/index.html"),
+        product: resolve(__dirname, "src/product_pages/index.html"),
+        productlisting: resolve(__dirname, "src/product-listing/index.html"),
+        checkout: resolve(__dirname, "src/checkout/index.html"),
+        checkedout: resolve(__dirname, "src/checkout/success.html"),
+        admin: resolve(__dirname, "src/admin/index.html"),
+      },
+    },
+  },
+});
